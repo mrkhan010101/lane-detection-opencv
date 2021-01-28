3238a6e9d0487cf2d88ee8a4044682dcda159d2e
Aaris-Kazi <aariskazi@gmail.com>
2021-01-28 19:28:20 +0530

    stage changes

--- lane_detect.py
+++ lane_detect.py
@@ -39,7 +39,7 @@ def show_lines(img, lines):
     line_image = np.zeros_like(img) # creating a copy of image with arrays of 0
     if lines is not None:
         for line in lines:
-            print(line)
+            # print(line)
             x1, y1, x2, y2 = line.reshape(4) # spliting 4 array element
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
