b7ebdf2f95498026b195684521ebbb14baeb2ccc
Aaris-Kazi <aariskazi@gmail.com>
2021-01-31 23:43:28 +0530

    stage changes for optimization

--- lane_test.py
+++ lane_test.py
@@ -14,31 +14,38 @@ def make_cordinates(image, parameter):
 def combo_lines(lane_image, lines):
     left_lane = []
     right_lane = []
-    for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
-        para = np.polyfit((x1, x2), (y1, y2), 1)
-        slope = para[0]
-        intercept = para[1]
-        if slope < 0:
-            left_lane.append((slope, intercept))
-        else:
-            right_lane.append((slope, intercept))
+    try:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            para = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = para[0]
+            intercept = para[1]
+            if slope < 0:
+                left_lane.append((slope, intercept))
+            else:
+                right_lane.append((slope, intercept))
 
-    left_avg = np.average(left_lane, axis=0)
-    right_avg = np.average(right_lane, axis=0)
-    left_line = make_cordinates(lane_image, left_avg)
-    right_line = make_cordinates(lane_image, right_avg)
-    return np.array([left_line, right_line])
+        left_avg = np.average(left_lane, axis=0)
+        right_avg = np.average(right_lane, axis=0)
+        left_line = make_cordinates(lane_image, left_avg)
+        right_line = make_cordinates(lane_image, right_avg)
+        return np.array([left_line, right_line])
+    
+    except Exception as e:
+        print(e)
+        
+    
 
 def show_lines(img, lines):
     line_image = np.zeros_like(img) # creating a copy of image with arrays of 0
-    if lines is not None:
+    try:
         for line in lines:
             # print(line)
             x1, y1, x2, y2 = line.reshape(4) # spliting 4 array element
             cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 10)
-    return line_image
-
+        return line_image
+    except Exception:
+        pass
 
 def area_of_interest(img):
     ht = img.shape[0]
@@ -55,7 +62,7 @@ def area_of_interest_video(img):
     ht = img.shape[0]
     # Co-ordinates of viewing triangele
     triangle = np.array([
-        [(0, ht), (2560, ht), (720, 256)]
+        [(0, ht), (2560, ht), (720, 50)]
     ])
     mask = np.zeros_like(img) # creating a copy of image with arrays of 0
     cv2.fillPoly(mask, triangle, 255) # function that create polygons of visible region
@@ -68,7 +75,7 @@ def capture(img):
     blur = cv2.GaussianBlur(gray, (5, 5), 0) # to reduce the noise 
     edges = cv2.Canny(blur, 50, 150) # to find the edges
     aoi = area_of_interest(edges)
-    lines = cv2.HoughLinesP(aoi, 2, np.pi/180, 100, np.array([]), 40, 5)
+    lines = cv2.HoughLinesP(aoi, 2, np.pi/180, 100, np.array([]), 40, 50)
     avg_lines = combo_lines(lane_image, lines)
     clines = show_lines(lane_image, avg_lines)
     color_image_line = cv2.addWeighted(lane_image, 0.8, clines, 1, 1) # to merge the output with the color image
@@ -85,16 +92,16 @@ def for_image():
 
 
 def for_video():
-    cap = cv2.VideoCapture('skate_park.mp4')
+    cap = cv2.VideoCapture('test2.mp4')
     while cap.isOpened():
         _, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # to convert the color from RGB to BW
         blur = cv2.GaussianBlur(gray, (5, 5), 0) # to reduce the noise 
         edges = cv2.Canny(blur, 50, 150) # to find the edges
         aoi = area_of_interest_video(edges)
-        lines = cv2.HoughLinesP(aoi, 2, np.pi/180, 100, np.array([]), 40, 5)
+        lines = cv2.HoughLinesP(aoi, 2, np.pi/180, 100, np.array([]), 40, 50)
         avg_lines = combo_lines(frame, lines)
-        clines = show_lines(frame, lines)
+        clines = show_lines(frame, avg_lines)
         color_image_line = cv2.addWeighted(frame, 0.9, clines, 1, 1)
         res = cv2.resize(color_image_line, (1280, 640))
         cv2.imshow('Window', res) # to show the output
