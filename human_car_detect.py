39ca17691a3ffa5d1993559156065ca2830e22a6
Aaris-Kazi <aariskazi@gmail.com>
2021-02-03 22:24:36 +0530

    can detect human body in a video

--- human_car_detect.py
+++ human_car_detect.py
@@ -1,15 +1,19 @@
 import cv2
-detection = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
-
-def image():
-    img = cv2.imread('image.jpg')
+def human_detector(img):
+    detection = cv2.CascadeClassifier('haarcascade_fullbody.xml')
     human = detection.detectMultiScale(img, 1.1, 1)
     for x, y, b, h in human:
         cv2.rectangle(img, (x, y), (x+b, y+h), (0, 255, 0), 2)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(img, 'Human', (x + 6, y - 6), font, 0.5, (0, 255, 0), 1)
+    return img
 
+
+def image():
+    img = cv2.imread('image.jpg')
+    human_detector(img)
+    
     res = cv2.resize(img, (360, 480))
     cv2.imshow('Frame', res)
     cv2.waitKey(0)
@@ -19,7 +23,11 @@ def video():
     cap = cv2.VideoCapture('people.mp4')
     while cap.isOpened():
         _, frame = cap.read()
-        cv2.imshow('Human Detection', cap)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        # blur = cv2.GaussianBlur(gray, (5,5), 0)
+        op = human_detector(frame)
+        res = cv2.resize(op, (720, 480))
+        cv2.imshow('Human Detection', res)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
