c0b0b2b3e501fddecb4a0a18c092d006d9ed83c3
Aaris-Kazi <aariskazi@gmail.com>
2021-02-15 14:45:09 +0530

    To see the dimension of the input file

---
+++ showDimensions.py
@@ -0,0 +1,7 @@
+def Dimensions(image, temp):
+    if temp ==0:
+        x = image.shape[0]
+        y = image.shape[1]
+        print(x, y)
+        temp = 1
+    return temp
--- test.py
+++ test.py
@@ -1,23 +1,21 @@
 import cv2
 import numpy as np
-import bezier as bz
+import time
+from fps import showfps
+from showDimensions import Dimensions
+temp = 0
+cap = cv2.VideoCapture('test_images/AMU.mp4')
+prev= time.time()
+fps = 0.0
 
-img= np.zeros((512, 512, 4), np.uint8)
-points = np.array([
-    [20,155], [250, 255], [120,155]
-], np.int32)
-points1 = np.asfortranarray([
-    [20, 120, 155], [250, 155, 255], 
-])
-points = points.reshape((-1, 1, 2))
-# points1 = points1.reshape((-1, 1, 2))
-curve = bz.Curve(points1,degree= 2)
-print(curve)
-while(True):
-    
-    # lines = cv2.line(img, (20,155), (200, 155), (255,0 ,255),5)
-    ply = cv2.polylines(img, [points], False, (255,0 ,255), 10 )
-    cv2.imshow('Testing window', ply)
+
+while cap.isOpened():
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    temp = Dimensions(gray, temp)
+    prev, fps = showfps(gray, prev, fps)
+    res = cv2.resize(gray, (400, 640))
+    cv2.imshow('Testing window', res)
     if cv2.waitKey(10) & 0xFF== ord('q'):
         break
 cv2.destroyAllWindows()
