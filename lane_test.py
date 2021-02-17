f089e41769b8793c1694f9d42457d336a57d1a1f
Aaris-Kazi <aariskazi@gmail.com>
2021-02-17 12:28:07 +0530

    minimize the code making a file

--- lane_test.py
+++ lane_test.py
@@ -1,67 +1,10 @@
 import cv2
 import numpy as np
-import math
 import time
 from fps import showfps
 from showDimensions import Dimensions
 from showLines import show_lines
-
-# Rapid Action in Directions
-def say_directions(left_line, right_line, lane_image):
-    x1, y1 = left_line.reshape(2)
-    x2, y2 = right_line.reshape(2)
-    print('%.2f'%math.tan(y1/x1), '%.2f'%math.tan(y2/x2))
-    font = cv2.FONT_HERSHEY_DUPLEX
-    l1, l2 = '', ''
-    if math.tan(y1/x1) < 0 and math.tan(y2/x2) < 0:
-        l1 = 'Right'
-    elif math.tan(y1/x1) > 0 and math.tan(y2/x2) > 0:
-        l2 = 'Left'
-    else:
-        cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
-        l1, l2 = 'Straight', 'Straight'
-
-    if l1 == 'Right' and l2 == 'Left':
-        cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
-    elif l1 == 'Straight' and l2 == 'Straight':
-        cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
-    elif l1 == 'Straight' or l1 == '' and l2 == 'Left':
-        cv2.putText(lane_image, 'Left', (26, 26), font, 0.5, (0, 255, 0), 1)
-    elif l1 == 'Right' and l2 == '' or l2 == 'Straight':
-        cv2.putText(lane_image, 'Right', (26, 26), font, 0.5, (0, 255, 0), 1)
-    
-        
-def make_cordinates(image, parameter):
-    try:
-        slope, intercept = parameter
-        y1 = image.shape[0]
-        y2 = int(y1*(4/5))
-        x1 = int((y1 - intercept)/slope)
-        x2 = int((y2 - intercept)/slope)
-        return np.array([x1, y1, x2, y2])
-    except Exception:
-        slope, intercept = 0.0, 0.0
-def combo_lines(lane_image, lines):
-    try:
-        left_lane = []
-        right_lane = []
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            para = np.polyfit((x1, x2), (y1, y2), 1)
-            slope = para[0]
-            intercept = para[1]
-            if slope < 0:
-                left_lane.append((slope, intercept))
-            else:
-                right_lane.append((slope, intercept))
-        left_avg = np.average(left_lane, axis=0)
-        right_avg = np.average(right_lane, axis=0)
-        left_line = make_cordinates(lane_image, left_avg)
-        right_line = make_cordinates(lane_image, right_avg)
-        say_directions(left_avg, right_avg, lane_image)
-    except Exception :
-        pass
-    return np.array([left_line, right_line])
+from show_combo_lines import combo_lines
 
 def area_of_interest(img):
     try:
@@ -75,7 +18,6 @@ def area_of_interest(img):
     except cv2.error:
         pass
     
-
 def area_of_interest_video(img):
     ht = img.shape[0] # Co-ordinates of viewing triangele
     triangle = np.array([
@@ -120,7 +62,7 @@ def for_video():
             blur = cv2.GaussianBlur(gray, (5, 5), 0) # to reduce the noise 
             edges = cv2.Canny(blur, 50, 150) # to find the edges
             aoi = area_of_interest_video(edges)
-            lines = cv2.HoughLinesP(aoi, 2, np.pi/180, 100, np.array([]), 40, 50)
+            lines = cv2.HoughLinesP(aoi, 2, np.pi/180, 100, np.array([]), 40, 5)
             avg_lines= combo_lines(frame, lines)
             clines = show_lines(frame, avg_lines)
             color_image_line = cv2.addWeighted(frame, 0.9, clines, 1, 1)
