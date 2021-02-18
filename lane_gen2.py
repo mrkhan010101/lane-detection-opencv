b47ea883fe6b5dd66ad73fa861123b08f0e66ace
Aaris-Kazi <aariskazi@gmail.com>
2021-02-18 15:58:13 +0530

    optimizing small code

--- lane_gen2.py
+++ lane_gen2.py
@@ -1,11 +1,9 @@
-from sys import path
 import cv2
 import numpy as np
 import glob
 
 def image():
     status = glob.glob('test_images/*.jpg')
-    print(status)
     if(status):
         for i in status:
             img = cv2.imread(i)
