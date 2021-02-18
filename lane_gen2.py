a0267c661dd5dcc330a3dc92df5ed9be1d87c1f1
Aaris-Kazi <aariskazi@gmail.com>
2021-02-18 20:55:01 +0530

    converting hsv for experiment

--- lane_gen2.py
+++ lane_gen2.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 import glob
 from showImageDimensions import dim
+
+def capture(img):
+    lane_image = np.copy(img)
+    hsv = cv2.cvtColor(lane_image, cv2.COLOR_BGR2HSV)
+    blur = cv2.GaussianBlur(hsv, (5, 5), 0)
+    edges = cv2.Canny(blur, 50, 150)
+    return edges
 def image():
     status = glob.glob('test_images/*.jpg')
     if(status):
@@ -9,7 +16,8 @@ def image():
             for i in status:
                 img = cv2.imread(i)
                 x, y = dim(img)
-                res = cv2.resize(img, (x-120, y-120))
+                cap = capture(img)
+                res = cv2.resize(cap, (x-120, y-120))
                 cv2.imshow('Debug', res)
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
