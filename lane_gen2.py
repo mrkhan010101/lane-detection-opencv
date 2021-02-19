5cb9027f5b64f31975d1d1f236d77e254ac4dd0a
Aaris-Kazi <aariskazi@gmail.com>
2021-02-19 15:41:35 +0530

    area of vision

--- lane_gen2.py
+++ lane_gen2.py
@@ -3,12 +3,27 @@ import numpy as np
 import glob
 from showImageDimensions import dim
 
+def area_of_interest(img):
+    try:
+        ht = img.shape[0]
+        wt = img.shape[1]
+        triangle = np.array([
+            [(0, ht-60), (wt, ht-60), (720, 420), (540, 420)]
+        ])
+        mask = np.zeros_like(img) # creating a copy of image with arrays of 0
+        cv2.fillPoly(mask, triangle, 255) # function that create polygons of visible region
+        masked_image = cv2.bitwise_and(img, mask) # it will hide other data and show only the visible part
+        return masked_image
+    except cv2.error:
+        pass
+
 def capture(img):
     lane_image = np.copy(img)
-    hsv = cv2.cvtColor(lane_image, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(hsv, (5, 5), 0)
     edges = cv2.Canny(blur, 50, 150)
-    return edges
+    aoi = area_of_interest(edges)
+    return aoi
 def image():
     status = glob.glob('test_images/*.jpg')
     if(status):
@@ -17,7 +32,7 @@ def image():
                 img = cv2.imread(i)
                 x, y = dim(img)
                 cap = capture(img)
-                res = cv2.resize(cap, (x-120, y-120))
+                res = cv2.resize(cap, (x-220, y-220))
                 cv2.imshow('Debug', res)
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
