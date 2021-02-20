60ff5d63f43b0329f473e0a812beba930a891cab
Aaris-Kazi <aariskazi@gmail.com>
2021-02-20 18:41:06 +0530

    system changes

--- show_combo_lines.py
+++ show_combo_lines.py
@@ -3,27 +3,32 @@ import numpy as np
 import math
 # Rapid Action in Directions
 def say_directions(left_line, right_line, lane_image):
-    x1, y1 = left_line.reshape(2)
-    x2, y2 = right_line.reshape(2)
-    print('%.2f'%math.tan(y1/x1), '%.2f'%math.tan(y2/x2))
-    font = cv2.FONT_HERSHEY_DUPLEX
-    l1, l2 = '', ''
-    if math.tan(y1/x1) < 0 and math.tan(y2/x2) < 0:
-        l1 = 'Right'
-    elif math.tan(y1/x1) > 0 and math.tan(y2/x2) > 0:
-        l2 = 'Left'
-    else:
-        cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
-        l1, l2 = 'Straight', 'Straight'
+    try:
+        x1, y1 = left_line.reshape(2)
+        x2, y2 = right_line.reshape(2)
+        print('%.2f'%math.tan(y1/x1), '%.2f'%math.tan(y2/x2))
+        font = cv2.FONT_HERSHEY_DUPLEX
+        l1, l2 = '', ''
+        if math.tan(y1/x1) < 0 and math.tan(y2/x2) < 0:
+            l1 = 'Right'
+            cv2.putText(lane_image, 'Right', (26, 26), font, 0.5, (0, 255, 0), 1)
+        elif math.tan(y1/x1) > 0 and math.tan(y2/x2) > 0:
+            l2 = 'Left'
+            cv2.putText(lane_image, 'Left', (26, 26), font, 0.5, (0, 255, 0), 1)
+        else:
+            cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
+            l1, l2 = 'Straight', 'Straight'
 
-    if l1 == 'Right' and l2 == 'Left':
-        cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
-    elif l1 == 'Straight' and l2 == 'Straight':
-        cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
-    elif l1 == 'Straight' or l1 == '' and l2 == 'Left':
-        cv2.putText(lane_image, 'Left', (26, 26), font, 0.5, (0, 255, 0), 1)
-    elif l1 == 'Right' and l2 == '' or l2 == 'Straight':
-        cv2.putText(lane_image, 'Right', (26, 26), font, 0.5, (0, 255, 0), 1)
+        # if l1 == 'Right' and l2 == 'Left':
+        #     cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
+        # elif l1 == 'Straight' and l2 == 'Straight':
+        #     cv2.putText(lane_image, 'Straight', (26, 26), font, 0.5, (0, 255, 0), 1)
+        # elif l1 == 'Straight' or l1 == '' and l2 == 'Left':
+        #     cv2.putText(lane_image, 'Left', (26, 26), font, 0.5, (0, 255, 0), 1)
+        # elif l1 == 'Right' and l2 == '' or l2 == 'Straight':
+        #     cv2.putText(lane_image, 'Right', (26, 26), font, 0.5, (0, 255, 0), 1)
+    except Exception as e:
+        print(e)
 def make_cordinates(image, parameter):
     try:
         slope, intercept = parameter
