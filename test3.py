8c5a14e74454d96778b46713f304202584a353fa
Aaris-Kazi <aariskazi@gmail.com>
2021-02-24 00:44:09 +0530

    code reducing

--- test3.py
+++ test3.py
@@ -8,26 +8,20 @@ from show_combo_lines import combo_lines
 from fps import showfps
 from showDimensions import  dims
 def filter_colors(image):
-	"""
-	Filter the image to include only yellow and white pixels
-	"""
-	# Filter white pixels
+    # Filter white pixels
 	white_threshold = 200 #130
 	lower_white = np.array([white_threshold, white_threshold, white_threshold])
 	upper_white = np.array([255, 255, 255])
 	white_mask = cv2.inRange(image, lower_white, upper_white)
 	white_image = cv2.bitwise_and(image, image, mask=white_mask)
-
 	# Filter yellow pixels
 	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 	lower_yellow = np.array([22,93,0])
 	upper_yellow = np.array([40,255,255])
 	yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 	yellow_image = cv2.bitwise_and(image, image, mask=yellow_mask)
-
 	# Combine the two above images
 	image2 = cv2.addWeighted(white_image, 1., yellow_image, 1., 0.)
-
 	return image2
 
 def area_of_interest(img):
@@ -92,7 +86,6 @@ def video():
             try:
                 _, frame = cap.read()
                 prev, fps = showfps(frame, prev, fps)
-                # gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # to convert the color from RGB to BW
                 hsv = filter_colors(frame)
                 temp = dims(hsv, temp)
                 blur = cv2.GaussianBlur(hsv, (5, 5), 0) # to reduce the noise 
@@ -113,5 +106,5 @@ def video():
     else:
         print('not exist')
 if __name__ == '__main__':
-    # image()
-    video()
+    image()
+    # video()
