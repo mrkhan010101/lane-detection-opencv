5f020980a5cfa1e923a41c3c93c46b2ec5c70181
Aaris-Kazi <aariskazi@gmail.com>
2021-03-29 00:36:58 +0530

    big optimization

--- show_combo_lines.py
+++ show_combo_lines.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+# from sklearn.linear_model import LinearRegression
 # Rapid Action in Directions
 from showDirections import say_directions
 def make_cordinates(image, parameter):
@@ -16,6 +16,7 @@ def combo_lines(lane_image, lines):
     try:
         left_lane = []
         right_lane = []
+        temp_left = np.array([ 75, 720, 466, 503])
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
             para = np.polyfit((x1, x2), (y1, y2), 1)
@@ -30,9 +31,14 @@ def combo_lines(lane_image, lines):
         right_avg = np.average(right_lane, axis=0)
         left_line = make_cordinates(lane_image, left_avg)
         right_line = make_cordinates(lane_image, right_avg)
-        print(left_line,"<< left|| right>>", right_line) # it was working
-        # if(left_line == None):
-        #     print('left was none') # it was working
+        # print(left_line,"<< left|| right>>", right_line) # it was working
+        
+        if(left_line == None):
+            left_line = temp_left.copy()
+            # print(left_line)
+            # print(temp_left)
+        temp_left = left_line.copy()
+        # print(temp_left)
         # say_directions(left_avg, right_avg, lane_image)
     except Exception:
         pass
--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-import cv2
-import numpy as np
-import time
-from fps import showfps
-from showDimensions import Dimensions
-def main():
-    temp = 0
-    cap = cv2.VideoCapture('test_images/AMU.mp4')
-    prev= time.time()
-    fps = 0.0
-
-    while cap.isOpened():
-        _, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        temp = Dimensions(gray, temp)
-        prev, fps = showfps(gray, prev, fps)
-        res = cv2.resize(gray, (400, 640))
-        cv2.imshow('Testing window', res)
-        if cv2.waitKey(10) & 0xFF== ord('q'):
-            break
-cv2.destroyAllWindows()
-if __name__ == '__main__':
-    main()
+import numpy as np                 
+  
+# creating the first array
+arr1 = np.array([2, 6, 9, 4])
+ 
+# displaying the identity of arr1
+# print(id(arr1))
+ 
+# shallow copy arr1 in arr2 using view()
+arr2 = arr1.copy()
+ 
+# displaying the identity of arr2
+# print(id(arr2))
+  
+# making a change in arr1
+arr1[1] = 7                       
+  
+# displayin the arrays
+print(arr1)
+print(arr2)
