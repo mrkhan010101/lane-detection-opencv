86f92b271c47409d8de7afdeba0117f7f3db9321
Aaris-Kazi <aariskazi@gmail.com>
2021-04-07 09:26:18 +0530

    filtering lines

--- combo_lines.py
+++ combo_lines.py
@@ -10,13 +10,16 @@ def combo(lane_image, lines):
     y2= []
     for line in lines:
         a1, b1, a2, b2 = line.reshape(4)
-        x1.append(a1)
-        y1.append(b1)
-        x2.append(a2)
-        y2.append(b2)
-        para = np.polyfit((a1,a2), (b1,b2), 1)
-        slope = para[0]
-        # print(slope)
+        if(b1 - b2 >= 50):
+            continue
+        else:
+            x1.append(a1)
+            y1.append(b1)
+            x2.append(a2)
+            y2.append(b2)
+            para = np.polyfit((a1,a2), (b1,b2), 1)
+            slope = para[0]
+            # print(slope)
     # df = pd.DataFrame({
     #     "X1": x1,
     #     "Y1": y1,
