08caf1a4ce2021f4f7f26c17f7192ec1c8df0812
Aaris-Kazi <aariskazi@gmail.com>
2021-04-07 11:44:32 +0530

    to debug the lines

--- combo_lines.py
+++ combo_lines.py
@@ -3,21 +3,24 @@ import pandas as pd
 import time
 def combo(lane_image, lines):
     # print(lane_image)
-    print(lines)
+    # print(lines)
     x1= []
     x2= []
     y1= []
     y2= []
     for line in lines:
-        a1, a2, b1, b2 = line.reshape(4)
+        a1, b1, a2, b2 = line.reshape(4)
         x1.append(a1)
-        x2.append(a2)
         y1.append(b1)
+        x2.append(a2)
         y2.append(b2)
+        para = np.polyfit((a1,a2), (b1,b2), 1)
+        slope = para[0]
+        print(slope)
     # df = pd.DataFrame({
     #     "X1": x1,
-    #     "X2": x2,
     #     "Y1": y1,
+    #     "X2": x2,
     #     "Y2": y2,
     # })
     i = time.time()
